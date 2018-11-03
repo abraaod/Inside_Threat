93e5eb7f9d1e6adbef69b4433a8bd98616b1d0ba
Henrique David de Medeiros <henriquemed101@gmail.com>
2018-11-03 19:16:54 -0300

    Adding Activity class and updating Http, Input and Logon class.

--- src/Http.java
+++ src/Http.java
@@ -1,51 +1,11 @@
 
-public class Http extends Node {
+public class Http extends Activity {
 	
-	String date;
-	String hour;
-	String user;
-	String device;
 	String url;
 	
 	public Http(String id, String date, String hour, String user, String device, String url) {
-		super(id);
-		this.date = date;
-		this.hour = hour;
-		this.user = user;
-		this.device = device;
+		super(id, date, hour, user, device);
 		this.url = url;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getHour() {
-		return hour;
-	}
-
-	public void setHour(String hour) {
-		this.hour = hour;
-	}
-
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public String getDevice() {
-		return device;
-	}
-
-	public void setDevice(String device) {
-		this.device = device;
 	}
 
 	public String getUrl() {
--- src/Input.java
+++ src/Input.java
@@ -1,51 +1,11 @@
 
-public class Input extends Node{
+public class Input extends Activity{
 	
-	String date;
-	String hour;
-	String user;
-	String device;
 	String activity;
 	
 	public Input(String id, String date, String hour, String user, String device, String activity) {
-		super(id);
-		this.date = date;
-		this.hour = hour;
-		this.user = user;
-		this.device = device;
+		super(id, date, hour, user, device);
 		this.activity = activity;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getHour() {
-		return hour;
-	}
-
-	public void setHour(String hour) {
-		this.hour = hour;
-	}
-
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public String getDevice() {
-		return device;
-	}
-
-	public void setDevice(String device) {
-		this.device = device;
 	}
 
 	public String getActivity() {
